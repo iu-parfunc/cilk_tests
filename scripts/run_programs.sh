1abad18adccd1308e28eb25cb595f57a31af3579
Ryan Newton <rrnewton@gmail.com>
2014-10-10 21:01:22 -0400

    Make run_programs fail if there are no programs to run.  However, this is pointless because we need to completely stop using this script to make HSbencher happy, at least when there is more than one binary per benchmark/Makefile.

--- scripts/run_programs.sh
+++ scripts/run_programs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-set -xe
+set -x
+set -e
 
 # No reason to do anything sophisticated here because if things
 # progress further we should move tests into the proper ICL test
@@ -9,6 +10,13 @@ set -xe
 # set -e 
 RUNARGS=$1
 
-for test in `ls bin | grep .exe`; do 
+EXES=`ls bin | grep .exe`
+
+if [ "$EXES" == "" ]; then 
+  echo 'Error: no binaries found!'
+  exit 1;
+fi
+
+for test in $EXES; do 
   ./bin/$test $RUNARGS
 done
