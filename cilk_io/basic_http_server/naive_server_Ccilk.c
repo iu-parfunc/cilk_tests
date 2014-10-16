fae46c24ef0e9934bc5ee3de6a6e4db60b3af6f9
Tim Zakian <tzakian@indiana.edu>
2014-10-16 10:11:56 -0400

    Small changes to naive Cilk server

--- cilk_io/basic_http_server/naive_server_Ccilk.c
+++ cilk_io/basic_http_server/naive_server_Ccilk.c
@@ -120,9 +120,7 @@ void acceptLoop()
 {
   int sd;
   struct sockaddr_in addr;
-  struct sockaddr_in caddr;
   socklen_t alen = sizeof(addr);
-  socklen_t clen = sizeof(caddr);
   short port = PORT_NUM;
   int sock_tmp;
   int current_worker = 0;
@@ -150,7 +148,7 @@ void acceptLoop()
   }
   while(1) {
     /*if (-1 == (sock_tmp = accept(sd, (struct sockaddr*)&addr, &alen))) {*/
-    if (-1 == (sock_tmp = cilk_accept(sd, (struct sockaddr*)&caddr, clen))) {
+    if (-1 == (sock_tmp = cilk_accept(sd, (struct sockaddr*)&addr, &alen))) {
       printf("Error %d doing accept", errno);
       exit(-1);
     }
