c9b7277a9d4b3692af2ad14a2ab3e44153cbb8ba
Abhishek Kulkarni <adkulkar@umail.iu.edu>
2014-10-19 02:10:34 -0400

    Updates to the http server run scripts

--- cilk_io/basic_http_server/run_with_env.sh
+++ cilk_io/basic_http_server/run_with_env.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-set -xe
+set -x
 
-export LD_LIBRARY_PATH=$HOME/opt/lib:$LD_LIBRARY_PATH
-export PATH=$HOME/opt/bin:$PATH
-export C_INCLUDE_PATH=$HOME/opt/include:$C_INCLUDE_PATH
-
-source ../../../build_scripts/env_cilk
-source ../../../build_scripts/set_up_cilk_iu
+export LD_LIBRARY_PATH=$1:$LD_LIBRARY_PATH
+shift
+export PATH=$1:$PATH
+shift
 
 $*
